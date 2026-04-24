e9dec97c59f984327b82f2389500744ac67e76b4
Anmo619 <chauhan18anmol@gmail.com>
2026-04-24 14:51:52 +0530

    fix port for render

--- server.js
+++ server.js
@@ -67,6 +67,8 @@ app.get("/api/companies", async (req, res) => {
 });
 
 // 🚀 Start server
-app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
